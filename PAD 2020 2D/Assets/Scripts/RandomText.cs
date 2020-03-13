91fe949c341a38e6202e41c2534623f8a0787ec0
Dani Baas <Dani.raj25@gmail.com>
2020-03-13 16:35:13 +0100

    From laptop to PC
    -Line will begin in beginpoint and drawn to origin (0, 0, 0)
    - Formula will be made from said line
    - Created static method to get formula from 2 vectors.

---
+++ PAD 2020 2D/Assets/Scripts/RandomText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomText : MonoBehaviour {
+
+    public Text formula;
+    public Vector3 beginPunt;
+    public Vector3 eindPunt;
+
+    // private ints for calculation
+    private int randomCoefficient;
+    private int startPos;
+    private string randomOperator;
+    private LineRenderer renderer;
+
+    // Start is called before the first frame update
+    void Start() {
+        formula = this.GetComponent<Text>();
+        renderer = FindObjectOfType<LineRenderer>();
+        beginPunt = new Vector3(-8, 4, 0);
+        formula.text = DrawLine.GetFormulaFromVector(beginPunt, eindPunt);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // draw line from data
+        // vul lengte in voor x
+        renderer.SetPosition(0, beginPunt);
+        renderer.SetPosition(1, eindPunt);
+    }
+}
