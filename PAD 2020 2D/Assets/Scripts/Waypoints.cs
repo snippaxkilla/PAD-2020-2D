e707814b12c34dc34b7b78017fbb352bd1ca6d86
Dani Baas <Dani.raj25@gmail.com>
2020-03-13 16:58:04 +0100

    Merged Level -> Dev

---
+++ PAD 2020 2D/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+    public static Transform[] pipes;
+
+    void Awake() {
+        pipes = new Transform[transform.childCount];
+        for (int i = 0; i < pipes.Length; i++) {
+            pipes[i] = transform.GetChild(i);
+        }
+    }
+}
